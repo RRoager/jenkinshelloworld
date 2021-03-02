ca1a2cc767eaeb232c2cfa5b7956e25e45b32b14
RRoager <rasmus-roager@hotmail.com>
2021-03-02 15:06:44 +0100

    Nyt nyt nyt

---
+++ src/test/java/tekno/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,15 @@
+package tekno.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
